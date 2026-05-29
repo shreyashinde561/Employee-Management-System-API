6278fd5166b3a76460c0246dfab226db4c8534a3
Shreya Shinde <shreyashinde561@gmail.com>
2026-05-29 13:56:41 +0530

    frontend connected to production api

--- employee-frontend/src/api/axios.js
+++ employee-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // ✅ FIXED PORT
+  baseURL: "https://employee-management-system-api-42ze.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
